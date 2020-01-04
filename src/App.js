e4150734a9ddd514001f7e7bff65e1319f440f08
dgbailey <bailey.dust@gmail.com>
2020-01-04 10:30:27 -0800

    fcomp theory

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import React from 'react';
+import React, {useEffect,useState,useRef} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+
+  //this differes from a counter becasuse we are dealing with a mutable date object
+  //usualy a functional component closes over its own state and props which coud lead to stale setInterval calls
+  //here we are not referencing state (a useState value) from a pervious rendering in our setInterval to build our new date
+  //this is why it continues to work
+  //set state probably doesn't mutate, but copy
+
+  const date = new Date();
+  const [hour,setHour] = useState(date.getHours());
+  const [minute,setMinute] = useState(date.getMinutes());
+  const [second,setSeconds] = useState(date.getSeconds());
+
+
+  function setNewTime(){
+
+      let newDate = new Date();
+      setHour(newDate.getHours());
+      setMinute(newDate.getMinutes());
+      setSeconds(newDate.getSeconds());
+
+
+  }
+
+  useEffect(() =>{
+
+    let interval = setInterval(setNewTime,1000);
+
+    return () => {
+      clearInterval(interval);
+    }
+
+  },[])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='Clock'>
+        <div className='display'>{hour}</div>
+        <div className='display'>{minute}</div>
+        <div className='display'>{second}</div>
+      </div>
+      
     </div>
   );
 }
