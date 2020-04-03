e5fc53b25c1b04aa7b2cc94420e1551b3def5ba4
dgbailey <bailey.dust@gmail.com>
2020-04-02 17:38:15 -0700

    Playing with intermediate reference type, useeffect, and useref

--- src/App.js
+++ src/App.js
@@ -1,53 +1,37 @@
-import React, {useEffect,useState,useRef} from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-
 function App() {
+	let [ count, setCount ] = useState(0);
+	let ref = useRef();
 
-  //this differes from a counter becasuse we are dealing with a mutable date object
-  //usualy a functional component closes over its own state and props which coud lead to stale setInterval calls
-  //here we are not referencing state (a useState value) from a pervious rendering in our setInterval to build our new date
-  //this is why it continues to work
-  //set state probably doesn't mutate, but copy
+	function incrementCounter() {
+		console.log('calling');
+		setCount(count + 1);
+	}
 
-  const date = new Date();
-  const [hour,setHour] = useState(date.getHours());
-  const [minute,setMinute] = useState(date.getMinutes());
-  const [second,setSeconds] = useState(date.getSeconds());
+	useEffect(() => {
+		function tick() {
+			ref.current = incrementCounter;
+		}
+		console.log('ticks');
+		tick();
+	});
 
+	useEffect(() => {
+		//big difference here between passing just ref.current and () => ref.current.  The former loses the intermediate reference type 'ref'
+		//so we just call the same function over and over again.  With () => ref.current, we still have ref providing mutable information that is used for the context of our next function call,
+		setInterval(() => ref.current, 1000);
+	}, []);
 
-  function setNewTime(){
-
-      let newDate = new Date();
-      setHour(newDate.getHours());
-      setMinute(newDate.getMinutes());
-      setSeconds(newDate.getSeconds());
-
-
-  }
-
-  useEffect(() =>{
-
-    let interval = setInterval(setNewTime,1000);
-
-    return () => {
-      clearInterval(interval);
-    }
-
-  },[])
-
-
-  return (
-    <div className="App">
-      <div className='Clock'>
-        <div className='display'>{hour}</div>
-        <div className='display'>{minute}</div>
-        <div className='display'>{second}</div>
-      </div>
-      
-    </div>
-  );
+	return (
+		<div className="App">
+			<div className="Clock">
+				<div className="display">{count}</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
